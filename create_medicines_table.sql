182d36da5879ed604a9cb0fcbf4b6541077ac25a
sakshi <162275297+sp-7385382427@users.noreply.github.com>
2025-07-14 23:23:28 +0530

    Add files via upload

---
+++ create_medicines_table.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE PharmacyDB;
+USE PharmacyDB;
+
+CREATE TABLE Medicines (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    brand VARCHAR(100),
+    price DOUBLE,
+    quantity INT
+);
+CREATE TABLE Medicines (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    brand VARCHAR(100),
+    price DOUBLE,
+    quantity INT
+);
+SELECT * FROM Medicines;
+
